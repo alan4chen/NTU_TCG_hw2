85010bab9be7e0569922e528b6dc67df4c774986
alan <alan4chen@kimo.com>
2016-12-08 04:58:32 +0800

    INIT

---
+++ othello_ver20161123/human_vs_cpu.sh
@@ -0,0 +1,4 @@
+g++ search.cpp -std=c++11 -O2 -Wall -o search
+echo compile_search_complete
+./search
+
---
+++ othello_ver20161123/test/pr.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  char a[10] = "abc";
+  cout << a + 5 << endl; 
+}
